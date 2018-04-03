0bcf84d312fbee4bc0200b30f19540d33327c27a
akhanani <sosoakha@gmail.com>
2018-04-03 09:57:16 +0200

    my bootcamp-tests

--- test/fromWhere.tests.js
+++ test/fromWhere.tests.js
@@ -1,9 +1,15 @@
-describe('fromWhere', function(){
+describe('fromWhere',function(){
 
-    it('return reg numbers from Bellville only', function(){
-        assert.equal(isFromBellville('CY'),true);
+    it('should returns regNumbers starts with CY',function(){
+        assert.equal(fromWhere('Bellville'),('Some other place!'),true);
     });
-    it('return reg numbers from Paarl', function(){
-        assert.equal(isFromBellville('CJ'),false);
+    it('should returns regNumbers for CJ',function(){
+        assert.equal(fromWhere('Paarl'),('Some other place!'),true);
+    });
+    it ('should returns regNumbers for CA',function(){
+      assert.equal(fromWhere('Cape Town'),('Some other place!'),true);
+    });
+    it ('should returns otherwise ',function(){
+    assert.equal(fromWhere('CR 143, CL 133'),('Some other place!'),true);
     });
 });
